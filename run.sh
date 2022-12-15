fa52e63eb157eaf065b5482724a5fd1a45931745
Nhu Hoang <59313380+geniusnhu@users.noreply.github.com>
2022-12-15 23:57:44 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,38 @@
+python run_speech_recognition_seq2seq_streaming.py \
+--model_name_or_path="openai/whisper-large-v2" \
+--dataset_name="mozilla-foundation/common_voice_11_0" \
+--dataset_config_name="vi" \
+--language="vietnamese" \
+--train_split_name="train+validation" \
+--eval_split_name="test" \
+--model_index_name="Whisper Large V2 Vietnamese" \
+--max_steps="5000" \
+--output_dir="./" \
+--per_device_train_batch_size="8" \
+--per_device_eval_batch_size="4" \
+--logging_steps="25" \
+--learning_rate="1e-7" \
+--warmup_steps="500" \
+--evaluation_strategy="steps" \
+--eval_steps="500" \
+--save_strategy="steps" \
+--save_steps="1000" \
+--generation_max_length="225" \
+--length_column_name="input_length" \
+--max_duration_in_seconds="30" \
+--text_column_name="sentence" \
+--freeze_feature_encoder="False" \
+--report_to="tensorboard" \
+--metric_for_best_model="wer" \
+--greater_is_better="False" \
+--gradient_accumulation_steps="3" \
+--load_best_model_at_end \
+--gradient_checkpointing \
+--fp16 \
+--overwrite_output_dir \
+--do_train \
+--do_eval \
+--predict_with_generate \
+--do_normalize_eval \
+--use_auth_token \
+--push_to_hub
